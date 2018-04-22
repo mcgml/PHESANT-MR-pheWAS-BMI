71a9af6d5635f4095910e6217a77ee14605c8c18
Matthew Lyon <ml@Matthews-MacBook-Pro.local>
2018-04-22 19:13:02 +0100

    added script to prep dirs

---
+++ make.dirs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+mkdir ~/2016-biobank-mr-phewas-bmi
+
+mkdir ~/2016-biobank-mr-phewas-bmi/data
+mkdir ~/2016-biobank-mr-phewas-bmi/data/sample500/
+mkdir ~/2016-biobank-mr-phewas-bmi/data/sample500/phenotypes
+mkdir ~/2016-biobank-mr-phewas-bmi/data/sample500/phenotypes/derived
+mkdir ~/2016-biobank-mr-phewas-bmi/data/sample500/phenotypes/original
+
+mkdir ~/2016-biobank-mr-phewas-bmi/results
+mkdir ~/2016-biobank-mr-phewas-bmi/results/sample500k
